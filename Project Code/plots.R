771fef7503f11aae51f3ae38f45950beaf11cbce
Joe B <92501130+joebd@users.noreply.github.com>
2023-05-31 07:26:25 -0500

    Update plots.R
    Added the other plots from the research.

--- Project Code/plots.R
+++ Project Code/plots.R
@@ -15,4 +15,31 @@ plot1 +
 
 # -------------------------------------------------------------------------
 
+plot2 <- plot_model(mod2a, type = "pred", terms = "partyid5") +
+  scale_y_continuous(labels = scales::percent, limits = c(0,1)) + 
+  scale_x_continuous(labels=c("1"="Strong \n Democrat", "2"="Weak \n Democrat",
+                              "3"="Independent", "4"="Weak \n Republican", 
+                              "5"="Strong \n Republican")) +
+  labs(x = "Party Identity", y = "Anti-Funding Attitudes", 
+       title = "") +
+  theme_classic()
 
+
+plot2
+
+
+plot3a <- plot_model(mod2a, type = "pred", terms = "intenvir_new") + 
+  scale_y_continuous(labels = scales::percent, limits = c(0,1)) + 
+  labs(x = "Enviromental Policy Interest Support", y = "Anti-Funding Attitudes", 
+       title = "") +
+  theme_classic()
+
+
+plot3b <- plot_model(mod2a, type = "pred", terms = "intmil_new") + 
+  scale_y_continuous(labels = scales::percent, limits = c(0,1)) + 
+  labs(x = "Millitary Policy Interest Support", y = "Anti-Funding Attitudes", 
+       title = "") +
+  theme_classic()
+
+plot3a
+plot3b
